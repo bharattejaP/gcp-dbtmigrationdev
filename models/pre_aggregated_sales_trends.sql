3ee7e1b6c5941c185dda47f07ac5a3f557f48c85
bharatteja puvvada <bpuvvada@innominds.com>
2025-02-27 09:24:03 +0000

    changes

--- models/pre_aggregated_sales_trends.sql
+++ models/pre_aggregated_sales_trends.sql
@@ -1,7 +1,8 @@
-CREATE OR REPLACE TABLE `retaildummy.myfourthmodel` AS SELECT 
+CREATE OR REPLACE TABLE `retaildummy.
+pre_aggregated_sales_trends` AS SELECT 
     d.FiscalYear_week AS FiscalWeek,
     ap.dimWarehouseKey AS Warehouse,
 SUM(ap.PickedPieces) AS TotalSales
 FROM retaildummy.vfactActualPieces ap
 JOIN retaildummy.vdimDate d ON ap.dimActualDayDateKey = d.dimDateKey
-GROUP BY FiscalWeek, Warehouse limit 10;
+GROUP BY FiscalWeek, dimWarehouseKey;
