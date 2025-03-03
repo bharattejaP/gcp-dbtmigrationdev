f969e5e555715660925a2ee5331187b150131b7c
bharatteja puvvada <bpuvvada@innominds.com>
2025-03-03 07:24:10 +0000

    changes

--- models/mv_factActualHours_Agg.sql
+++ models/mv_factActualHours_Agg.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE VIEW retaildummy.vfactActualHours_Agg AS
+CREATE OR REPLACE VIEW `mimetic-setup-451414-e7.retaildummy.vfactActualHours_Agg AS
 SELECT
     PARSE_DATE('%Y%m%d', CAST(dimWorkDayDateKey AS STRING)) AS dimWorkDayDateKey, 
     dimWarehouseKey,
@@ -24,5 +24,5 @@ SELECT
     SUM(IHVacationOH) AS TotalIHVacationOH,
     SUM(IHOtherProd) AS TotalIHOtherProd,
     SUM(IHOtherOH) AS TotalIHOtherOH
-FROM retaildummy.vfactActualHours
+FROM `mimetic-setup-451414-e7.retaildummy.vfactActualHours
 GROUP BY dimWorkDayDateKey, dimWarehouseKey, dimWarehouseHierarchyKey;
